17a0ac02301478d7ad8fcd4f806b38b4f72de5f7
Greench1108 <1839848600@qq.com>
2020-01-07 00:07:18 -0800

    added a file

---
+++ ex2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int count(char str[]){
+    int i = 0;
+    while(str[i] != '\0'){
+      i++;
+    }
+    return i;
+}
+
+void reverse(char input[], char result[]) {
+    int length = count(input) - 1;
+    int index = 0;
+    while(length >= 0){
+      result[index++] = input[length--];
+    }
+}
+
+int main()
+{
+    char input[1000];
+    int i = 0;
+    input[i] = getchar();
+    while (input[i] !='\n'){
+        i++;
+        input[i] = getchar();
+    }
+
+    char result[1000];
+    reverse(input, result);
+    printf("%s\n", result);
+}
